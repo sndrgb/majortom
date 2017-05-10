ceac301af1b48ef79c9ab92adf5cfb7885c9ab32
Alessandro <alessandrorigobello@aol.com>
2017-05-10 23:37:34 +0200

    added score at the end

--- src/App.vue
+++ src/App.vue
@@ -43,6 +43,10 @@
             <div v-if="show">
                 <div class="mid" v-if="status === 'gameover' || status === 'paused'">
                     <p>Game Over</p>
+                    <h2>
+                        You run for<br>
+                        <span>{{Math.floor(game.game.distance)}}</span>
+                    </h2>
                     <button @click="replay">
                         Replay
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M12 0c3.31 0 6.29 1.353 8.46 3.522l2.48-2.48L24 8.382l-7.437-.965 2.49-2.49C17.242 3.122 14.752 2 12 2 6.486 2 2 6.486 2 12s4.486 10 10 10c3.872 0 7.23-2.216 8.89-5.443l1.717 1.046C20.595 21.406 16.602 24 12 24 5.373 24 0 18.627 0 12S5.373 0 12 0z"/></svg>
